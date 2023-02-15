1d684dbd724970282788b07bf3576778b773b622
John Watson <johnwatson484@hotmail.com>
2023-02-15 21:23:08 +0000

    Change topic

--- app/messaging/schema.js
+++ app/messaging/schema.js
@@ -4,9 +4,9 @@ module.exports = Joi.object({
   specversion: Joi.string().required(),
   type: Joi.string().required(),
   source: Joi.string().required(),
-  subject: Joi.string().required(),
-  id: Joi.string().required(),
+  id: Joi.string().uuid().required(),
   time: Joi.date().required(),
+  subject: Joi.string().optional(),
   datacontenttype: Joi.string().optional(),
   data: Joi.any().optional()
 }).required()
