ab7c42c5a856c8c6885462540220ef520e46c13d
John Watson <johnwatson484@hotmail.com>
2023-02-15 19:53:33 +0000

    Add schema

--- app/messaging/schema.js
+++ app/messaging/schema.js
@@ -1,3 +1,12 @@
 const Joi = require('joi')
 
-module.exports = Joi.object().required()
+module.exports = Joi.object({
+  specversion: Joi.string().required(),
+  type: Joi.string().required(),
+  source: Joi.string().required(),
+  subject: Joi.string().required(),
+  id: Joi.string().required(),
+  time: Joi.date().required(),
+  datacontenttype: Joi.string().optional(),
+  data: Joi.any().optional()
+}).required()
