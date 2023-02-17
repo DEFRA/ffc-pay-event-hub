ca3dc92a633e526a74581289e5e1dd0fde0aaf1a
John Watson <john.watson2@rpa.gov.uk>
2023-02-17 11:36:07 +0000

    Add payment schema

--- app/inbound/schemas/payment.js
+++ app/inbound/schemas/payment.js
@@ -1,5 +1,7 @@
 const Joi = require('joi')
 
 module.exports = Joi.object({
-
+  frn: Joi.number().integer().required(),
+  correlationId: Joi.string().guid().required(),
+  schemeId: Joi.number().integer().required()
 })
