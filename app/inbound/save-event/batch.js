6fff7ea181923e7896d3889b99f65d9fb6ed3c2c
John Watson <johnwatson484@hotmail.com>
2023-02-18 20:17:24 +0000

    Add events

--- app/inbound/save-event/batch.js
+++ app/inbound/save-event/batch.js
@@ -1,4 +1,18 @@
+const { BATCH_EVENT } = require('../../constants/event-types')
+const { getClient } = require('../../storage')
+const { getTimestamp } = require('./get-timestamp')
+
 const saveBatchEvent = async (event) => {
+  const timestamp = getTimestamp(event.time)
+  const batchEntity = {
+    partitionKey: event.data.filename,
+    rowKey: timestamp.toString(),
+    ...event,
+    data: JSON.stringify(event.data)
+  }
+
+  const client = getClient(BATCH_EVENT)
+  await client.createEntity(batchEntity)
 }
 
 module.exports = {
