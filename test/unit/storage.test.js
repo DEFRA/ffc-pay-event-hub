1489ec1766541be4632f7e0a1a0cd00535787ae1
John Watson <john.watson2@rpa.gov.uk>
2023-03-22 15:06:00 +0000

    Add storage tests

--- test/unit/storage.test.js
+++ test/unit/storage.test.js
@@ -1,5 +1,5 @@
-jest.mock('../../app/config/storage')
-const mockConfig = require('../../app/config/storage')
+const config = require('../../app/config/storage')
+const { PAYMENT_EVENT, BATCH_EVENT, HOLD_EVENT, WARNING_EVENT } = require('../../app/constants/event-types')
 
 const mockTableClient = {
   createTable: jest.fn()
@@ -18,36 +18,59 @@ const { initialiseTables, getClient } = require('../../app/storage')
 describe('storage', () => {
   beforeEach(() => {
     jest.clearAllMocks()
-    mockConfig.useConnectionString = true
-    mockConfig.connectionString = 'test-connection-string'
-    mockConfig.paymentTable = 'payments'
-    mockConfig.batchTable = 'batches'
-    mockConfig.holdTable = 'holds'
-    mockConfig.warningTable = 'warnings'
   })
 
-  test('should create payment table from connection string if use connection string true', async () => {
+  test('should create payment table when initialising tables', async () => {
     await initialiseTables()
-    expect(mockTableClient.createTable).toHaveBeenCalledWith(mockConfig.paymentTable)
+    expect(mockTableClient.createTable).toHaveBeenCalledWith(config.paymentTable)
   })
 
-  test('should create batches table from connection string if use connection string true', async () => {
+  test('should create batches table when initialising tables', async () => {
     await initialiseTables()
-    expect(mockTableClient.createTable).toHaveBeenCalledWith(mockConfig.batchTable)
+    expect(mockTableClient.createTable).toHaveBeenCalledWith(config.batchTable)
   })
 
-  test('should create hold table from connection string if use connection string true', async () => {
+  test('should create hold table when initialising tables', async () => {
     await initialiseTables()
-    expect(mockTableClient.createTable).toHaveBeenCalledWith(mockConfig.holdTable)
+    expect(mockTableClient.createTable).toHaveBeenCalledWith(config.holdTable)
   })
 
-  test('should create warning table from connection string if use connection string true', async () => {
+  test('should create warning table when initialising tables', async () => {
     await initialiseTables()
-    expect(mockTableClient.createTable).toHaveBeenCalledWith(mockConfig.warningTable)
+    expect(mockTableClient.createTable).toHaveBeenCalledWith(config.warningTable)
   })
 
-  test('should create warning table from connection string once per table if use connection string true', async () => {
+  test('should create each table once', async () => {
     await initialiseTables()
     expect(mockTableClient.createTable).toHaveBeenCalledTimes(4)
+  })
+
+  test('getClient should return payment client if payment event', async () => {
+    await initialiseTables()
+    const client = getClient(PAYMENT_EVENT)
+    expect(client).toBeDefined()
+  })
+
+  test('getClient should return batch client if payment event', async () => {
+    await initialiseTables()
+    const client = getClient(BATCH_EVENT)
+    expect(client).toBeDefined()
+  })
+
+  test('getClient should return hold client if payment event', async () => {
+    await initialiseTables()
+    const client = getClient(HOLD_EVENT)
+    expect(client).toBeDefined()
+  })
+
+  test('getClient should return warning client if payment event', async () => {
+    await initialiseTables()
+    const client = getClient(WARNING_EVENT)
+    expect(client).toBeDefined()
+  })
+
+  test('getClient should throw error for unknown event', async () => {
+    await initialiseTables()
+    expect(() => getClient('unknown')).toThrow()
   })
 })
