1368c5a287030851ff0daf27c93135c5d6d28f21
John Watson <john.watson2@rpa.gov.uk>
2023-03-22 13:50:37 +0000

    Add hold event

--- app/inbound/save-event/hold.js
+++ app/inbound/save-event/hold.js
@@ -1,4 +1,16 @@
+
+const { FRN, SCHEME_ID } = require('../../constants/categories')
+const { HOLD_EVENT } = require('../../constants/event-types')
+const { getClient } = require('../../storage')
+const { createEntity } = require('./create-entity')
+
 const saveHoldEvent = async (event) => {
+  const frnBasedEntity = createEntity(event.data.frn, event.data.schemeId, FRN, event)
+  const schemeIdBasedEntity = createEntity(event.data.schemeId, event.data.frn, SCHEME_ID, event)
+
+  const client = getClient(HOLD_EVENT)
+  await client.createEntity(frnBasedEntity)
+  await client.createEntity(schemeIdBasedEntity)
 }
 
 module.exports = {
--- app/inbound/schemas/hold.js
+++ app/inbound/schemas/hold.js
@@ -1,5 +1,6 @@
 const Joi = require('joi')
 
 module.exports = Joi.object({
-  schemeId: Joi.number().integer().required()
+  schemeId: Joi.number().integer().required(),
+  frn: Joi.number().integer().required()
 })
