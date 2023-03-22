916e6975ead5c12f2a4ea33a6217616f9e278314
John Watson <johnwatson484@hotmail.com>
2023-03-22 20:05:15 +0000

    Add tests

--- app/inbound/schemas/hold.js
+++ app/inbound/schemas/hold.js
@@ -1,6 +1,6 @@
 const Joi = require('joi')
 
 module.exports = Joi.object({
-  schemeId: Joi.number().integer().required(),
-  frn: Joi.number().integer().required()
+  schemeId: Joi.number().integer().positive().required(),
+  frn: Joi.number().integer().positive().required()
 })
---
+++ test/unit/inbound/schemas/payment.test.js
@@ -0,0 +1,83 @@
+const schema = require('../../../../app/inbound/schemas/payment')
+
+let event
+
+describe('payment schema', () => {
+  beforeEach(() => {
+    event = JSON.parse(JSON.stringify(require('../../../mocks/payment-event').data))
+  })
+
+  test('should validate a valid payment event', () => {
+    expect(schema.validate(event).error).toBeUndefined()
+  })
+
+  test('should not validate a payment event with an undefined frn', () => {
+    delete event.frn
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a null frn', () => {
+    event.frn = null
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a non-number frn', () => {
+    event.frn = 'abc'
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a non-integer frn', () => {
+    event.frn = 1.1
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a negative frn', () => {
+    event.frn = -1
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with an undefined correlationId', () => {
+    delete event.correlationId
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a null correlationId', () => {
+    event.correlationId = null
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a non-string correlationId', () => {
+    event.correlationId = 1
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a non-uuid correlationId', () => {
+    event.correlationId = 'abc'
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with an undefined schemeId', () => {
+    delete event.schemeId
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a null schemeId', () => {
+    event.schemeId = null
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a non-number schemeId', () => {
+    event.schemeId = 'abc'
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a non-integer schemeId', () => {
+    event.schemeId = 1.1
+    expect(schema.validate(event).error).toBeDefined()
+  })
+
+  test('should not validate a payment event with a negative schemeId', () => {
+    event.schemeId = -1
+    expect(schema.validate(event).error).toBeDefined()
+  })
+})
