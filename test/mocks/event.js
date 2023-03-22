136c4133fa475fd8cf18a8ce86132a741e1b443c
John Watson <johnwatson484@hotmail.com>
2023-03-22 19:23:08 +0000

    Add tests

--- test/mocks/event.js
+++ test/mocks/event.js
@@ -5,5 +5,6 @@ module.exports = {
   id: 'f39deb76-bd15-4532-8efb-92783800847e',
   time: new Date().toISOString(),
   subject: 'Payment event',
-  datacontenttype: 'text/json'
+  datacontenttype: 'text/json',
+  data: 'data'
 }
---
+++ test/unit/inbound/process-event.test.js
@@ -0,0 +1,36 @@
+jest.mock('../../../app/inbound/get-event-type')
+const { getEventType: mockGetEventType } = require('../../../app/inbound/get-event-type')
+
+jest.mock('../../../app/inbound/save-event')
+const { saveEvent: mockSaveEvent } = require('../../../app/inbound/save-event')
+
+jest.mock('../../../app/inbound/validate-event-data')
+const { validateEventData: mockValidateEventData } = require('../../../app/inbound/validate-event-data')
+
+const event = require('../../mocks/event')
+
+const MOCK_EVENT_TYPE = 'mock-event-type'
+
+const { processEvent } = require('../../../app/inbound/process-event')
+
+describe('process event', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+    mockGetEventType.mockReturnValue(MOCK_EVENT_TYPE)
+  })
+
+  test('should get the event type from event type', async () => {
+    await processEvent(event)
+    expect(mockGetEventType).toHaveBeenCalledWith(event.type)
+  })
+
+  test('should validate event data with event type', async () => {
+    await processEvent(event)
+    expect(mockValidateEventData).toHaveBeenCalledWith(event.data, MOCK_EVENT_TYPE)
+  })
+
+  test('should save the event', async () => {
+    await processEvent(event)
+    expect(mockSaveEvent).toHaveBeenCalledWith(event, MOCK_EVENT_TYPE)
+  })
+})
