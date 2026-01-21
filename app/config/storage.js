81a0fa1c13ae059634c8bc58c9261e8f59a55071
Oliver Lewington <oliver.lewington@atos.net>
2026-01-21 15:30:34 +0000

    Adding config and cache

--- app/config/storage.js
+++ app/config/storage.js
@@ -8,6 +8,9 @@ const schema = Joi.object({
   holdTable: Joi.string().default('holds'),
   warningTable: Joi.string().default('warnings'),
   batchTable: Joi.string().default('batches'),
+  container: Joi.string().default('reports'),
+  dataRequestContainer: Joi.string().default('data-requests'),
+  createEntities: Joi.bool().default(false),
   managedIdentityClientId: Joi.string().optional()
 })
 
@@ -19,6 +22,9 @@ const config = {
   holdTable: process.env.AZURE_STORAGE_HOLD_TABLE,
   warningTable: process.env.AZURE_STORAGE_WARNING_TABLE,
   batchTable: process.env.AZURE_STORAGE_BATCH_TABLE,
+  container: process.env.AZURE_STORAGE_CONTAINER,
+  dataRequestContainer: process.env.AZURE_STORAGE_DATA_REQUEST_CONTAINER,
+  createEntities: process.env.AZURE_STORAGE_CREATE_ENTITIES,
   managedIdentityClientId: process.env.AZURE_CLIENT_ID
 }
 
