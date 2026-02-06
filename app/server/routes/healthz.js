7dc6ebe7c6fc2627b192a8bf000a2a1acee1632d
Oliver Lewington <oliver.lewington@atos.net>
2026-02-06 14:30:33 +0000

    Upping coverage

--- app/server/routes/healthz.js
+++ app/server/routes/healthz.js
@@ -1,10 +1,10 @@
 const { GET } = require('../../constants/methods')
-const { OK } = require('../../constants/http-status')
+const { SUCCESS } = require('../../constants/http-status')
 
 module.exports = {
   method: GET,
   path: '/healthz',
   handler: (_request, h) => {
-    return h.response('ok').code(OK)
-  }
+    return h.response('ok').code(SUCCESS)
+  },
 }
