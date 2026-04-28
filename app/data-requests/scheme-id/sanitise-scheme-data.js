88d6681385499cf8077733a012ed5ec7c58dc711
Leigh Godson <leigh.godson@atos.net>
2026-04-28 15:40:25 +0100

    add guard to previous flip code

--- app/data-requests/scheme-id/sanitise-scheme-data.js
+++ app/data-requests/scheme-id/sanitise-scheme-data.js
@@ -4,7 +4,7 @@ const schemeNames = require('../../constants/scheme-names')
 const showNonAccountingValue = (value) => {
   const cleaned = value.replaceAll(/[£,]/g, '')
   const amount = Number.parseFloat(cleaned)
-  const flipped = -amount
+  const flipped = amount === 0 ? amount : -amount
   return `£${flipped.toLocaleString('en-GB', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`
 }
 
