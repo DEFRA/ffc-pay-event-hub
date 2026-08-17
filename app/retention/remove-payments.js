af94bbab69324eff258908a65c9961e34930ba28
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2026-08-17 10:57:21 +0100

    Refine retention process to correctly clear batch, frn events based on payments removed (#70)

--- app/retention/remove-payments.js
+++ app/retention/remove-payments.js
@@ -3,16 +3,74 @@ const db = require('../data')
 const removePayments = async (agreementNumber, frn, schemeId, usesContractNumber, transaction) => {
   const agreementKey = usesContractNumber ? 'contractNumber' : 'agreementNumber'
 
+  const where = {
+    [db.Sequelize.Op.and]: [
+      db.Sequelize.where(
+        db.sequelize.json(`data.${agreementKey}`),
+        agreementNumber
+      ),
+      db.Sequelize.where(
+        db.Sequelize.literal("(data->>'frn')::int"),
+        Number(frn)
+      ),
+      db.Sequelize.where(
+        db.Sequelize.literal("(data->>'schemeId')::int"),
+        Number(schemeId)
+      )
+    ]
+  }
+
+  let batches = []
+  let agreementNumbers = []
+  let correlationIds = []
+
+  if (usesContractNumber) {
+    const paymentsToDelete = await db.payments.findAll({
+      attributes: [
+        [db.Sequelize.literal("data->>'batch'"), 'batch'],
+        [db.Sequelize.literal("data->>'agreementNumber'"), 'agreementNumber'],
+        [db.Sequelize.literal("data->>'correlationId'"), 'correlationId']
+      ],
+      where,
+      raw: true,
+      transaction
+    })
+
+    batches = [
+      ...new Set(
+        paymentsToDelete
+          .map(payment => payment.batch)
+          .filter(Boolean)
+      )
+    ]
+
+    agreementNumbers = [
+      ...new Set(
+        paymentsToDelete
+          .map(payment => payment.agreementNumber)
+          .filter(Boolean)
+      )
+    ]
+
+    correlationIds = [
+      ...new Set(
+        paymentsToDelete
+          .map(payment => payment.correlationId)
+          .filter(Boolean)
+      )
+    ]
+  }
+
   await db.payments.destroy({
-    where: {
-      [db.Sequelize.Op.and]: [
-        db.Sequelize.where(db.sequelize.json(`data.${agreementKey}`), agreementNumber),
-        db.Sequelize.where(db.Sequelize.literal('(data->>\'frn\')::int'), Number(frn)),
-        db.Sequelize.where(db.Sequelize.literal('(data->>\'schemeId\')::int'), Number(schemeId))
-      ]
-    },
+    where,
     transaction
   })
+
+  return {
+    batches,
+    agreementNumbers,
+    correlationIds
+  }
 }
 
 module.exports = {
