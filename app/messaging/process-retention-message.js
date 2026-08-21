8e8ffc022e8a894e4a1d834dd9fa6a929029ec6b
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2026-08-21 12:32:33 +0100

    add ignore retention messages flag (#72)

--- app/messaging/process-retention-message.js
+++ app/messaging/process-retention-message.js
@@ -1,18 +1,23 @@
+const { messageConfig } = require('../config')
 const { removeAgreementData } = require('../retention')
 
 const processRetentionMessage = async (message, receiver) => {
-  try {
-    const retentionData = message.body
-    console.log(`Agreement ${retentionData.agreementNumber} for FRN ${retentionData.frn} has passed retention period`)
-    await removeAgreementData(retentionData)
+  if (messageConfig.ignoreRetentionMessages) {
     await receiver.completeMessage(message)
-    console.log('Data related to agreement removed where present')
-  } catch (err) {
-    console.error('Unable to process retention data:', err)
+  } else {
     try {
-      await receiver.deadLetterMessage(message)
-    } catch (deadLetterErr) {
-      console.error('Failed to dead letter message:', deadLetterErr)
+      const retentionData = message.body
+      console.log(`Agreement ${retentionData.agreementNumber} for FRN ${retentionData.frn} has passed retention period`)
+      await removeAgreementData(retentionData)
+      await receiver.completeMessage(message)
+      console.log('Data related to agreement removed where present')
+    } catch (err) {
+      console.error('Unable to process retention data:', err)
+      try {
+        await receiver.deadLetterMessage(message)
+      } catch (deadLetterErr) {
+        console.error('Failed to dead letter message:', deadLetterErr)
+      }
     }
   }
 }
